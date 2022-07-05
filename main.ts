b3246911bb8a747b47c67816dbc3902ef150367e
YuDeTsai <108704003+YuDeTsai@users.noreply.github.com>
2022-07-05 14:57:21 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == 1) {
+        basic.showString("A")
+    }
+})
+let LastValue = 0
+bluetooth.startLEDService()
+basic.showString("Hello!")
+let Angle = 90
+pins.servoWritePin(AnalogPin.P1, Angle)
+basic.forever(function () {
+	
+})
